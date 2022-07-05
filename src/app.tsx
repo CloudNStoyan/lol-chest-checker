4ad66bb95bec84b0e3a777fd2773bad2f18273da
Stoyan Kolev <slownessbg@gmail.com>
2022-07-05 22:08:45 +0300

    refactoring global styles

--- src/app.tsx
+++ src/app.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import StartComponent from "./Components/StartComponent";
-import GlobalStyled from "./Global";
+import GlobalStyled from "./Components/styles/GlobalStyled";
 
 const App = () => {
   return (
